7bd9faa8e06da42a7de2525c9f052dcdf2815309
Burhan Uddin <bunnub5683@gmail.com>
2022-03-01 01:43:59 +0600

    few error handling done

--- js/app.js
+++ js/app.js
@@ -12,6 +12,11 @@ document.querySelector('#search-btn').addEventListener('click', () => {
             .then(response => response.json())
             .then(info => loadData(info.data));
         phoneContainer.innerHTML = '';
+        document.getElementById('error-msg').innerText = '';
+    } else {
+        document.getElementById('body').classList.add('default-view');
+        phoneContainer.innerHTML = '';
+        document.getElementById('error-msg').innerText = 'Please enter something to search';
     }
     document.getElementById('search-text').value = '';
 });
@@ -19,7 +24,6 @@ document.querySelector('#search-btn').addEventListener('click', () => {
 const phoneContainer = document.getElementById('phn-container');
 
 const loadData = (data) => {
-    // console.log(data);
     data.forEach(info => {
         // will display only 20 results
         if (data.indexOf(info) < 20) {
@@ -29,11 +33,16 @@ const loadData = (data) => {
             <div class="my-3">
                 <h5 class="card-title">${info.phone_name}</h5>
                 <p class="card-text">Brand : ${info.brand}</p>
-                <a href="#" class="btn btn-primary">Details</a>
+                <a href="#" onClick = 'showDetails(${JSON.stringify(info.slug)})' class="btn btn-primary">Details</a>
             </div>
-            `;
+            `;//onClick = 'showDetails(${idName})' info.slug.toString()
             div.classList.add('info-container', 'col-lg-3', 'col-md-5', 'col-sm-10', 'mx-auto');
             phoneContainer.appendChild(div);
         }
     })
 }
+
+//show details
+const showDetails = (idName) => {
+    console.log(idName);//slugname
+};
