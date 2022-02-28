fe9cc7c0aad94701f95af84b8e31f09c7e8db12a
Burhan Uddin <bunnub5683@gmail.com>
2022-03-01 01:23:00 +0600

    api dynamic done

--- js/app.js
+++ js/app.js
@@ -4,11 +4,36 @@
 // window.onload = () => {
 //     document.getElementById('body').classList.add('default-view');
 // }
-// document.querySelector('#search-btn').addEventListener('click', () => {
-//     const searchValue = document.getElementById('search-text').value.toLowerCase();
-//     document.getElementById('body').classList.remove('default-view');
-//     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
-//         .then(response => response.json())
-//         .then(data => console.log(data.data))
-//     document.getElementById('search-text').value = '';
-// });
+document.querySelector('#search-btn').addEventListener('click', () => {
+    const searchValue = document.getElementById('search-text').value.toLowerCase();
+    if (searchValue !== '') {
+        document.getElementById('body').classList.remove('default-view');
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
+            .then(response => response.json())
+            .then(info => loadData(info.data));
+        phoneContainer.innerHTML = '';
+    }
+    document.getElementById('search-text').value = '';
+});
+
+const phoneContainer = document.getElementById('phn-container');
+
+const loadData = (data) => {
+    // console.log(data);
+    data.forEach(info => {
+        // will display only 20 results
+        if (data.indexOf(info) < 20) {
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <img class="mt-3" style="width:100%; border-radius: 20px;" src='${info.image}'>
+            <div class="my-3">
+                <h5 class="card-title">${info.phone_name}</h5>
+                <p class="card-text">Brand : ${info.brand}</p>
+                <a href="#" class="btn btn-primary">Details</a>
+            </div>
+            `;
+            div.classList.add('info-container', 'col-lg-3', 'col-md-5', 'col-sm-10', 'mx-auto');
+            phoneContainer.appendChild(div);
+        }
+    })
+}
